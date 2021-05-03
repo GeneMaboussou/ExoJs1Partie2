6315ee51c244443fded2edfb4ae513110fb2d506
GeneMaboussou <mgeneray@yahoo.fr>
2021-05-03 12:32:32 +0200

    corrigé ExoJs1Partie2

--- script/script.js
+++ script/script.js
@@ -1,7 +1,8 @@
+document.getElementById("image1").addEventListener("mouseover",
 function setNewImage(){
-document.getElementById('image1').src="images/Merryl2.jpg";
-}
-
+    document.getElementById('image1').src="images/Merryl2.jpg";
+})
+document.getElementById("image1").addEventListener("mouseout",
 function setOldImage(){
     document.getElementById('image1').src="images/Merryl1.jpg";
-}
+})
