0f6bbcf1a33a1c176d6e7184ad87fac53adddd1e
GeneMaboussou <mgeneray@yahoo.fr>
2021-05-03 10:44:44 +0200

    ExoJs1Partie2

---
+++ script/script.js
@@ -0,0 +1,7 @@
+function setNewImage(){
+document.getElementById('image1').src="images/Merryl2.jpg";
+}
+
+function setOldImage(){
+    document.getElementById('image1').src="images/Merryl1.jpg";
+}
